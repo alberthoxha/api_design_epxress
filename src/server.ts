2dc46f032906335cdd35fd778dd505390ba8d0ea
albert <dev.alberthoxha@gmail.com>
2024-08-15 18:42:07 +0200

    first commit

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import cors from "cors";
+import express from "express";
+import morgan from "morgan";
+import { createNewUser, signin } from "./handlers/user";
+import { protect } from "./modules/auth";
+import router from "./router";
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", protect, router);
+
+app.post("/user", createNewUser);
+app.post("/signin", signin);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.json({ message: `had an error: ${err.message}` });
+});
+
+export default app;
