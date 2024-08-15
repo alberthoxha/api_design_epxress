ffcdd214228fb63bc10f6d9ab36044f40afd886b
albert <dev.alberthoxha@gmail.com>
2024-08-15 19:00:39 +0200

    add home dir

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,10 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.json({ message: "hello from the app" });
+});
+
 app.use("/api", protect, router);
 
 app.post("/user", createNewUser);
