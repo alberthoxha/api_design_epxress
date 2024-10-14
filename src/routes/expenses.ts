0e2983e1516169d8e6a5a8adce50b8f5c04c577e
Albert Hoxha <dev.alberthoxha@gmail.com>
2024-10-14 18:05:58 +0200

    remove swagger from global router

--- src/routes/expenses.ts
+++ src/routes/expenses.ts
@@ -3,22 +3,6 @@ import expensesController from '../controllers/expensesController'
 
 const expensesRouter = Router()
 
-/**
- * @swagger
- * /api/expenses:
- *   get:
- *     summary: Returns all expense
- *     tags: [Books]
- *     responses:
- *       200:
- *         description: the list of expense
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/PrismaClient'
- */
 expensesRouter.get('/', expensesController.getAllExpenses)
 expensesRouter.get('/:id', expensesController.getExpenseById)
 expensesRouter.post('/', expensesController.createExpense)
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
 import cors from 'cors'
-import express, { Express, Request, Response } from 'express'
+import express, { Express, Router } from 'express'
 import morgan from 'morgan'
 import swaggerUi from 'swagger-ui-express'
 import { authenticate } from './middlewares/auth'
 import userRouter from './routes/user'
 import { swaggerSpec } from './swagger/swagger'
 
-import prisma from './prisma/client'
 import expensesRouter from './routes/expenses'
 
 const app = express()
+
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
@@ -18,15 +18,6 @@ app.use(express.urlencoded({ extended: true }))
 export const setupSwagger = (app: Express): void => {
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 }
-
-app.get('/', async (_: Request, res: Response): Promise<void> => {
-  try {
-    const users = await prisma.user.findMany()
-    res.json(users)
-  } catch (error: any) {
-    res.status(500).json({ error: error.message })
-  }
-})
 
 app.use('/user', userRouter)
 app.use('/api/expenses', authenticate, expensesRouter)
