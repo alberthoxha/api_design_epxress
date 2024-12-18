d3a407e33aa2028936943f55022105275d6aad82
alberthoxha <dev.alberthoxha@gmail.com>
2024-12-18 10:54:16 +0100

    test api without auth

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,13 @@
-import { Router } from 'express'
+import { Request, Response, Router } from 'express'
 import expensesController from '../controllers/expensesController'
 import userController from '../controllers/userController'
 import { authenticate } from '../middlewares/auth'
 
 const api = Router()
+
+api.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'Hello there' })
+})
 
 api.post('/user/signup', userController.createNewUser)
 api.post('/user/login', userController.login)
