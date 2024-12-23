e23ee546e9d25e299a5a88391c86fe915084e422
alberthoxha <dev.alberthoxha@gmail.com>
2024-12-23 23:32:25 +0100

    Refactor authentication middleware to improve type safety and simplify JWT handling

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,7 +1,8 @@
 import { User } from '@prisma/client'
 import bcrypt from 'bcrypt'
-import { NextFunction } from 'express'
+import { NextFunction, Response } from 'express'
 import jwt from 'jsonwebtoken'
+import { UserRequest } from '../_types/types'
 
 export const comparePasswords = (password: string, hash: string): Promise<boolean> => {
   return bcrypt.compare(password, hash)
@@ -13,39 +14,22 @@ export const hashPassword = (password: string): Promise<string> => {
 
 export const createJWT = (user: User, expiresIn: string = '1h'): string => {
   const secret = process.env.JWT_SECRET
-
   if (!secret) {
     throw new Error('JWT_SECRET is not defined in the environment variables')
   }
-
-  const token = jwt.sign(
-    {
-      id: user?.id,
-      email: user?.email,
-    },
-    secret,
-    { expiresIn }
-  )
-
+  const token = jwt.sign({ id: user?.id, email: user?.email }, secret, { expiresIn })
   return token
 }
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const authenticate = (req: any, res: any, next: NextFunction) => {
-  const bearer = req.headers.authorization
+export const authenticate = (req: UserRequest, res: Response, next: NextFunction): any => {
+  const bearer = req.headers.authorization?.split(' ')[1]
+
   if (!bearer) {
     return res.status(401).json({ message: 'not authorized' })
   }
 
-  const [, token] = bearer.split(' ')
-
-  if (!token) {
-    return res.status(401).json({ message: 'not valid token' })
-  }
-
   try {
-    const user = jwt.verify(token, process.env.JWT_SECRET!)
-    req.user = user
+    req.user = jwt.verify(bearer, process.env.JWT_SECRET!) as jwt.JwtPayload as any
     next()
   } catch (e) {
     console.error(e)
